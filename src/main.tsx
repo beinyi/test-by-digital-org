ef68bbdfbe237e11d5197b7b112de5ba327d39ad
beinyi <eponchincev2011@gmail.com>
2024-12-20 12:07:11 +0700

    chore: eslint, tsconfig, app folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./app/index.css";
 import "@fontsource/inter";
-import App from "./App.tsx";
+import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
